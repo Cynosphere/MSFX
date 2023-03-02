e7b1999994a96cc2fd7cfa8b21c1acc5cd104097
Cynthia Foxwell <gamers@riseup.net>
2023-03-01 18:54:13 -0700

    Initial commit 2

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # MSFX
+Fork of [the MSFX Firefox userChrome](https://github.com/WinClassic/MSFX).
+![Screenshot of MSFX](.assets/screenshot.png)
 
-Fork of the MSFX Firefox userChrome
+## Changes
+* Uses normal [firefox-scripts](https://github.com/xiaoxiaoflood/firefox-scripts) instead of a stripped down version for greater userscript compatibility
+* More accurate styles in some places
+  * Bookmarks bar folder menus
+  * All tabs button
+    * Replaced with overflow arrows icon
+  * Scrollbars forced to light theme
+  * Selected address bar forced to light theme
+  * Resize grabber no longer appears when maximized
+  * Autoplay Pending indicator icon replaced
+* Pinned tabs no longer have extra padding
+* Internet status bar widget is its own component
+  * Empty boxes have been removed in favor of putting extensions on the status bar
+* Address bar is no longer forced to the Address toolbar when opening a new window
+
+## Installation
+* Set the following keys in `about:config`:
+  * `svg.context-properties.content.enabled` to `true`
+  * `ui.prefersReducedMotion` to `1`
+  * `browser.display.windows.non_native_menus` to `0`
+* Follow [firefox-scripts](https://github.com/xiaoxiaoflood/firefox-scripts) installation.
+* Install [Status Bar userscript](https://raw.githubusercontent.com/xiaoxiaoflood/firefox-scripts/master/chrome/status-bar.uc.js).
+* Merge `chrome` folder from the repo into your profile's `chrome` folder.
+
+## Recommendations
+* [Extensions Options Menu](https://raw.githubusercontent.com/xiaoxiaoflood/firefox-scripts/master/chrome/extensionOptionsMenu.uc.js)
+  * More lightweight than the stock extensions menu, which is disabled by the theme anyways
+* [userChromeJS Manager](https://raw.githubusercontent.com/xiaoxiaoflood/firefox-scripts/master/chrome/rebuild_userChrome.uc.js)
+* [Save File to](https://raw.githubusercontent.com/xiaoxiaoflood/firefox-scripts/master/extensions/savefileto/savefileto.xpi)
+  * **Requires extensions support in firefox-scripts**
+  * "Replaces" save dialog for hybrid classic theme setup with Windhawk until investigation is done to fix the save dialog on classic enabled programs.
---
+++ chrome/create_internet_on_status_bar.uc.js
@@ -0,0 +1,69 @@
+Components.utils.import("resource:///modules/CustomizableUI.jsm");
+var { Services } = Components.utils.import(
+  "resource://gre/modules/Services.jsm",
+  {}
+);
+var sss = Components.classes[
+  "@mozilla.org/content/style-sheet-service;1"
+].getService(Components.interfaces.nsIStyleSheetService);
+
+var IE6StatusBar = {
+  init: function () {
+    try {
+      // create a default toolbar button
+      CustomizableUI.createWidget({
+        id: "ie6_statusbar", // button id
+        defaultArea: CustomizableUI.AREA_NAVBAR,
+        removable: true,
+        label: "Internet", // button title
+        tooltiptext: "", // tooltip title
+        onCreated: function (button) {
+          return button;
+        },
+      });
+
+      // style button icon / embedded non-animated icon, because there is no image for then inside Fx anymore
+      var uri = Services.io.newURI(
+        "data:text/css;charset=utf-8," +
+          encodeURIComponent(
+            `#ie6_statusbar {
+  display: flex;
+  width: 151px;
+  height: 18px;
+  box-shadow: inset -1px -1px 0 ThreeDHighlight, inset 1px 1px 0 ThreeDShadow;
+}
+
+#ie6_statusbar::before {
+  content: "";
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  margin-right: 3px;
+  background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAeUExURYGBgf///8HBwQD/AACBAAAAhQAA/wAAAABmnAAAALYrxDEAAAAKdFJOU////////////wCyzCzPAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAYUlEQVQoU3XM0Q6AUAgCUEv05v//cGDWWls8yWHT6pMfMKWvBrNtdwAxYLu7eoZEADiSPVeDZXJNdoDSoETyzxvUB4CDnX8GoEl/bihtwXRvYAQRA3W1WOoNVUvpa+BJ1Qlz1waLzqVQ1QAAAABJRU5ErkJggg==");
+}
+
+#ie6_statusbar::after {
+  content: "Internet";
+  display: inline-block;
+  text-shadow: none !important;
+}
+#ie6_statusbar > .toolbarbutton-icon {
+  display: none;
+}`
+          ),
+        null,
+        null
+      );
+
+      // remove old style sheet, before registering the new one
+      if (sss.sheetRegistered(uri, sss.AGENT_SHEET)) {
+        sss.unregisterSheet(uri, sss.AGENT_SHEET);
+      }
+      sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+    } catch (e) {
+      Components.utils.reportError(e);
+    }
+  },
+};
+
+document.addEventListener("DOMContentLoaded", IE6StatusBar.init(), false);
